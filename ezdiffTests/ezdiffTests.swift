e9ca7b0018c9320dd81c5677b1c55ebfd4b7e1b5
Deep Mandloi <mandloideep22@gmail.com>
2026-03-06 19:38:48 -0600

    Initial Commit

---
+++ ezdiffTests/ezdiffTests.swift
@@ -0,0 +1,17 @@
+//
+//  ezdiffTests.swift
+//  ezdiffTests
+//
+//  Created by Deep Mandloi on 3/6/26.
+//
+
+import Testing
+@testable import ezdiff
+
+struct ezdiffTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ ezdiffUITests/ezdiffUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ezdiffUITestsLaunchTests.swift
+//  ezdiffUITests
+//
+//  Created by Deep Mandloi on 3/6/26.
+//
+
+import XCTest
+
+final class ezdiffUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
